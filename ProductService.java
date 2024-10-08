b89d8b628f68cb158fe188d13ba2b644fd6cddf1
Deepthi0076 <137717012+Deepthi0076@users.noreply.github.com>
2024-10-09 00:11:45 +0530

    Update ProductService.java

--- ProductService.java
+++ ProductService.java
@@ -1,8 +1,23 @@
-package com.example.Service;
+package com.scaler.firstspringapi.models;
 
-public interface ProductService {
-	
-	
-}
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Product extends BaseModel {
+    private String title;
+    private String description;
+    private double price;
+    private String image;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn
+    private Category category;
+    private int qty;
+    private int numberOfOrders;
 }
