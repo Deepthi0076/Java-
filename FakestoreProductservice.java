22eda9c93796517ee38dbff99bd21fa134b91f72
Deepthi0076 <137717012+Deepthi0076@users.noreply.github.com>
2024-10-09 00:13:53 +0530

    Update Category java

--- FakestoreProductservice.java
+++ FakestoreProductservice.java
@@ -1,5 +1,20 @@
-package com.example.Service;
+package com.scaler.firstspringapi.models;
 
-public class FakestoreProductservice {
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Category extends BaseModel{
+    private String title;
+//    @OneToMany(mappedBy = "category") // (fetch = FetchType.EAGER)
+//    private List<Product> products;
 }
